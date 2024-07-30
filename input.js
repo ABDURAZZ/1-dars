ce8a2320dcb52684a0d5ea650403160bd93ad9af
ABDURAZZ <abdurazzokbossov@gmail.com>
2024-07-30 10:05:05 +0500

    Add files via upload

---
+++ input.js
@@ -0,0 +1,15 @@
+let name =  prompt('Isim kiritin')
+let age =  prompt('yosh kiritin')
+let shool =  prompt('maktab nomini kritin')
+let manzil =  prompt('manzilingizni kritin')
+let famil =  prompt( 'familiangizni kritin  ')
+let yil_sana =  prompt('yilingizni kritin')
+let marstagi_ustozingiz =  prompt(' marstagi ustozingiz')
+console.log(name);
+console.log(age );
+console.log(shool );
+console.log(manzil);
+console.log(famil );
+console.log(yil_sana);
+console.log( marstagi_ustozingiz);
+
